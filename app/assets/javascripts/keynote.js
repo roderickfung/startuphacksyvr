d5ecdf0a20b7055e92d3403606821816a82241f0
Roderick <roderickfung@gmail.com>
2017-02-06 03:14:25 -0800

    ninja 1: updated keynote hover jquery + added sponsor section

--- app/assets/javascripts/keynote.js
+++ app/assets/javascripts/keynote.js
@@ -3,9 +3,19 @@ $(document).ready(function(){
 
     $('.square').mouseover(function(){
       $(this).addClass('blue');
+      // $('.hoverlay').css('display', 'block');
+      // $('.hoverlay').css('opacity', '1');
     }).mouseleave(function(){
       $(this).removeClass('blue');
+      // $('.hoverlay').css('display', 'none');
+      // $('.hoverlay').css('opacity', '0');
     })
+
+    // $('.keynote-image').mouseover(function(){
+    //   $(this).addClass('hoverlay');
+    // }).mouseleave(function(){
+    //   $(this).removeClass('hoverlay');
+    // })
 
   })
 })
